83a5d0c2cec92918684c20e16dfb2f475e50ee81
jungwoos <jungwoos@users.noreply.github.com>
2020-04-13 02:23:10 +0900

    second commit
    detail page

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,11 +2,12 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "d6a6ddd9b7c6f58e07e55527345d8fb4",
-"main.dart.js": "1c7bac687777235b83da0bc8b4cc9df4",
+"/": "d6a6ddd9b7c6f58e07e55527345d8fb4",
+"main.dart.js": "e1abb57f2c849442355fb1b119b28032",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "201e463de18f3730d830ac41a05dc563",
-"assets/LICENSE": "9d8717a95504b702e57f440c31e5cf56",
+"assets/LICENSE": "418ace862995c139a6fa61d5c15b227d",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
